fbbd1e57922c9b83bfefaf22c41c9a1e2b817d4f
srs-sudeep <sudeepranjan@iitbhilai.ac.in>
2023-01-14 00:38:16 +0530

    frontend

--- filesharing/lib/main.dart
+++ filesharing/lib/main.dart
@@ -80,15 +80,18 @@
 //   //   throw Exception('Failed to create album.');
 //   // }
 // }
-import 'dart:html';
-import 'dart:typed_data';
+// import 'dart:html';
+// import 'dart:typed_data';
+
+// import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:http/http.dart' as http;
-import 'dart:io';
-import 'dart:convert';
-
+import 'package:clipboard/clipboard.dart';
+// import 'dart:io';
+// import 'dart:convert';
+import 'package:flutter_share/flutter_share.dart';
 import 'package:http/http.dart';
 
 void main() => runApp(MyApp());
@@ -112,51 +115,164 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<void> share() async {
+    await FlutterShare.share(
+        title: 'Example share',
+        text: 'Example share text',
+        linkUrl: 'https://flutter.dev/',
+        chooserTitle: 'Example Chooser Title'
+    );
+  }
+  var download = " ";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('File Sharing App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(
-              child: Text('Upload File'),
-              onPressed: () async {
-                // Select a file using the file picker plugin
-                var result =
-                    await FilePicker.platform.pickFiles(type: FileType.any);
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "FILE SHARING APP",
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.visible,
+                style: const TextStyle(
+                    overflow: TextOverflow.fade,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue,
+                    fontSize: 40,
+                    shadows: <Shadow>[
+                Shadow(
+                offset: Offset(2.0, 2.0),
+                blurRadius: 3.0,
+                color: Color.fromARGB(255, 0, 0, 0),
+              ),
+              Shadow(
+                offset: Offset(2.0, 2.0),
+                blurRadius: 8.0,
+                color: Color.fromARGB(125, 0, 0, 255),
+              ),
+            ],),
 
-                if (result != null) {
-                  var request = MultipartRequest(
-                      'POST', Uri.parse('http://10.3.17.54:8000/upload'));
-                  request.files.add(await http.MultipartFile.fromPath(
-                    'file',
-                    result.files.first.path.toString(),
-                  ));
+              ),
+              SizedBox(height: 100),
+              ElevatedButton(
+                child: Text(
+                  'UPLOAD FILE',
+                  style: TextStyle(fontWeight: FontWeight.bold,overflow: TextOverflow.visible,),
+                ),
+                style: ButtonStyle(
+                  textStyle:
+                      MaterialStateProperty.all(const TextStyle(fontSize: 23)),
+                  overlayColor: MaterialStateProperty.all(Colors.red),
+                  shadowColor: MaterialStateProperty.all(Colors.lightBlue),
+                  elevation: MaterialStateProperty.all(15),
+                  minimumSize: MaterialStateProperty.all(const Size(200, 80)),
+                  // splashFactory: InkSparkle.constantTurbulenceSeedSplashFactory,
+                ),
+                onPressed: () async {
+                  // Select a file using the file picker plugin
+                  var result =
+                      await FilePicker.platform.pickFiles(type: FileType.any);
 
-                  // Send a POST request to the server to upload the file
-                  var response = await request.send();
-                  print(response.statusCode);
-                  print(request.files.first.field);
-                }
-              },
-            ),
-            ElevatedButton(
-              child: Text('Download File'),
-              onPressed: <String> () async {
-    final response = await http.get(Uri.parse('http://10.3.17.54:8000/download'));
-    if (response.statusCode == 200) {
-    print(response.body);
-    // Text(response.body);
-    } else {
-    throw Exception('Failed to get string');
-    }
-    }
-            ),
-          ],
+                  if (result != null) {
+                    var request = MultipartRequest(
+                        'POST', Uri.parse('http://10.3.9.249:8000/upload'));
+                    request.files.add(await http.MultipartFile.fromPath(
+                      'file',
+                      result.files.first.path.toString(),
+                    ));
+
+                    // Send a POST request to the server to upload the file
+                    var response = await request.send();
+                    print(response.statusCode);
+                    print(request.files.first.field);
+                  }
+                },
+              ),
+              SizedBox(height: 100),
+              Center(
+                child: Row(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.fromLTRB(10,10,10,10),
+                        constraints: BoxConstraints(
+
+                            minHeight: 50,
+                            minWidth: 50,
+                            maxWidth: 400,
+                            maxHeight: 70),
+                        decoration:BoxDecoration(
+                          // border: Border(
+                          //   top: BorderSide(),
+                          //   left: BorderSide(),
+                          //   right: BorderSide(),
+                          //   bottom: BorderSide(),
+                          // ),
+                          border: Border.all(
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                        child: Text(download)),
+                    GestureDetector(
+                      child: const Icon(Icons.copy, color: Colors.blue, size: 32),
+                      onTap: () {
+                        if (download.trim() == "") {
+                          print('enter text');
+                        } else {
+                          FlutterClipboard.copy(download).then((value) {
+                            const snack = SnackBar(
+                                content: Text("Text copied"),
+                                duration: Duration(seconds: 2));
+                            ScaffoldMessenger.of(context).showSnackBar(snack);
+                          });
+                        }
+                        ;
+                      },
+                    ),
+                    IconButton(
+                      onPressed:share,
+                      icon: Icon(Icons.share,
+                          color: Colors.blue, size: 32
+
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                  child: Text(
+                    'DOWNLOAD FILE',
+                    style: TextStyle(fontWeight: FontWeight.bold,overflow: TextOverflow.visible,),
+                  ),
+                  style: ButtonStyle(
+                    textStyle:
+                        MaterialStateProperty.all(const TextStyle(fontSize: 23)),
+                    overlayColor: MaterialStateProperty.all(Colors.red),
+                    shadowColor: MaterialStateProperty.all(Colors.lightBlue),
+                    elevation: MaterialStateProperty.all(15),
+                    minimumSize: MaterialStateProperty.all(const Size(200, 80)),
+                  ),
+                  onPressed: <String>() async {
+                    final response = await http
+                        .get(Uri.parse('http://10.3.9.249:8000/download'));
+                    if (response.statusCode == 200) {
+                      print(response.body);
+                      setState(() {
+                        download = response.body;
+                      });
+                      // Text(response.body);
+                    } else {
+                      throw Exception('Failed to get string');
+                    }
+                  }),
+            ],
+          ),
         ),
       ),
     );
